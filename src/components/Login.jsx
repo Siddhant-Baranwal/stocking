cb2f697528a045f6fe45e087ee42930bfadcf0c8
sksmagr23 <sakshamag34@gmail.com>
2024-09-24 19:27:36 +0530

    modified the form ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = ({ toggleForm }) => {
     };
 
     return (
-        <div className="rounded-lg border-white border-opacity-10 border-2 shadow-2xl p-6 max-w-lg w-full backdrop-blur-md" style={{ backgroundImage: 'radial-gradient(circle 500px at 50% 50%, #134074, #00072d)' }}>
+        <div className="rounded-lg border-white border-opacity-10 border-2 p-6 max-w-lg w-full backdrop-blur-lg shadow-[0_0_15px_rgba(255,255,255,0.2)]">
             <h2 className="text-2xl text-center mb-10 text-white font-bold">
                 Login to your account
             </h2>
@@ -26,7 +26,7 @@ const Login = ({ toggleForm }) => {
                         className={`peer w-full p-3 border ${errors.email ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
                         placeholder="Enter your email"
                     />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white">
+                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
                         Enter your Email {errors.email && <span className="text-red-500">*</span>}
                     </label>
                 </div>
@@ -38,28 +38,29 @@ const Login = ({ toggleForm }) => {
                         className={`peer w-full p-3 border ${errors.password ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
                         placeholder="Enter your password"
                     />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white">
+                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
                         Enter your Password {errors.password && <span className="text-red-500">*</span>}
                     </label>
                 </div>
 
                 {Object.keys(errors).length > 0 && (
-                    <div className="mb-4 text-gray-300 text-sm">
+                    <div className="mb-4 text-red-500 text-sm">
                         <p><span className="text-red-500">*</span> This is a required field</p>
                     </div>
                 )}
 
-                <button type='submit' className="w-full bg-blue-700 text-white p-3 rounded-xl hover:bg-sky-800 transform duration-300 shadow-xl font-semibold">
+                <button type='submit' className="w-full bg-blue-800 text-white p-3 rounded-xl hover:bg-blue-950 transform duration-300 shadow-xl font-semibold">
                     Login
                 </button>
             </form>
 
-            <div className="mt-4 text-center">
-                <p className="text-zinc-400">
+            <div className="mt-4 flex justify-between text-sm mx-2">
+                <p className='text-zinc-300 underline hover:text-blue-500'> Forgot Password</p>
+                <p className="text-zinc-300">
                     Don't have an account?
                     <span
                         onClick={toggleForm}
-                        className="text-blue-400 cursor-pointer hover:text-red-600 hover:underline transition-colors duration-300 ml-2">
+                        className="text-blue-400 cursor-pointer hover:text-red-600 underline transition-colors duration-300 ml-1">
                         Sign Up
                     </span>
                 </p>
