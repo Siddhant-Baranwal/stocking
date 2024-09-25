92d04cf157c2ef244bf91397e799fb7352514196
vivekmaurya001 <vm8118134@gmail.com>
2024-09-25 21:44:25 +0530

    Connected login and sign up with backend and maded auth context

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,85 +1,201 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { Modal, Space, Input, Typography, Flex, notification } from "antd";
+const { Title } = Typography;
 
 const Login = ({ toggleForm }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const onSubmit = (data) => {
-        console.log(data);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [OTP, setOtp] = useState("");
+  const timeoutDuration = 60000; // 1 minute in milliseconds (60 seconds)
+
+  // Function that returns a Promise and waits for the OTP to be entered
+  const waitForOtp = () => {
+    return new Promise((resolve, reject) => {
+      // Start checking every 500ms if OTP is entered
+      const checkOtp = setInterval(() => {
+        // Convert OTP to a string and check if it has the required length
+        if (OTP.length === 6) {
+          clearInterval(checkOtp); // Stop checking once OTP is entered
+          clearTimeout(timeout); // Clear the timeout if OTP is entered in time
+          resolve(OTP); // Resolve the promise with the OTP value
+        }
+      }, 500);
+
+      // Set a timeout for 1 minute, after which the promise is rejected
+      const timeout = setTimeout(() => {
+        clearInterval(checkOtp); // Stop checking if the time runs out
+        reject(new Error("OTP input timed out after 1 minute")); // Reject with an error
+      }, timeoutDuration);
+    });
+  };
+
+  const connectUrl = async (str, obj) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
+    let payload = {
+      email: obj.email,
+      password: obj.password,
+      name: obj.username,
+    };
+    const { data } = await axios.post(
+      `http://localhost:5000/api/auth/${str}`,
+      payload,
+      config
+    );
+    return data;
+  };
 
-    return (
-        <div className="rounded-lg border-white border-opacity-10 border-2 p-6 max-w-lg w-full backdrop-blur-lg shadow-[0_0_15px_rgba(255,255,255,0.2)]">
-            <h2 className="text-2xl text-center mb-10 text-white font-bold">
-                Login to your account
-            </h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="relative mb-4">
-                    <input
-                        type="email"
-                        {...register('email', {
-                            required: 'Email is required',
-                            pattern: {
-                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-                            }
-                        })}
-                        className={`peer w-full p-3 border ${errors.email ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
-                        placeholder="Enter your email"
-                    />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
-                        Enter your Email {errors.email && <span className="text-red-500">*</span>}
-                    </label>
-                </div>
+  const onSubmit = async (obj) => {
+    try {
+      // fetching the user in authentication db
+      const UserinAdb = await connectUrl("signin", obj);
+      console.log(UserinAdb);
 
-                <div className="relative mb-4">
-                    <input
-                        type="password"
-                        {...register('password', { required: 'Password is required' })}
-                        className={`peer w-full p-3 border ${errors.password ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
-                        placeholder="Enter your password"
-                    />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
-                        Enter your Password {errors.password && <span className="text-red-500">*</span>}
-                    </label>
-                </div>
+      if (UserinAdb.emailVerified) {
+        //fetching the user from DB
+        setIsModalOpen(true);
+        // const User = await connectUrl("FetchUser", obj);
+        const User = {};
+        // Process to chk Is user.code===OTP
+        const enteredOtp = await waitForOtp();
+        // converting cypher in userdb to otp
+        //doing process
+        if (enteredOtp === User.cypher) {
+          localStorage.setItem("userinfo", JSON.stringify(User));
+          notification.success({
+            message: "2nd Verification done!",
+            description: "Verified through QR succesfuly!",
+          });
+          navigate("/dashboard");
+        } else {
+          notification.error({
+            message: "Verification fialed!",
+          });
+          console.log("hi");
+          setIsModalOpen(false);
+          OTP = null;
+        }
+      } else {
+        notification.error({
+          message: "Invalid Credentials!",
+          description: "Invalid Email/password  Retry!",
+        });
+      }
+    } catch (error) {
+      notification.error({
+        message: "Error Occcured!",
+        description: error,
+      });
+    }
+  };
 
-                {Object.keys(errors).length > 0 && (
-                    <div className="mb-4 text-red-500 text-sm">
-                        <p><span className="text-red-500">*</span> This is a required field</p>
-                    </div>
-                )}
-
-                <button type='submit' className="w-full bg-blue-800 text-white p-3 rounded-xl hover:bg-blue-950 transform duration-300 shadow-xl font-semibold">
-                    Login
-                </button>
-            </form>
-
-            <div className="mt-4 flex justify-between text-sm mx-2">
-                <p className='text-zinc-300 underline hover:text-blue-500'> Forgot Password</p>
-                <p className="text-zinc-300">
-                    Don't have an account?
-                    <span
-                        onClick={toggleForm}
-                        className="text-blue-400 cursor-pointer hover:text-red-600 underline transition-colors duration-300 ml-1">
-                        Sign Up
-                    </span>
-                </p>
-            </div>
-
-            <div className="my-4 flex items-center">
-                <hr className="flex-grow border-t border-gray-300" />
-                <span className="px-4 text-gray-400">or</span>
-                <hr className="flex-grow border-t border-gray-300" />
-            </div>
-
-            <button className="w-full bg-sky-950 border border-gray-500 text-gray-100 p-2 rounded-lg hover:bg-blue-900 transform duration-300 shadow-2xl font-semibold">
-                <span className='flex justify-center items-center gap-2'>
-                    <img src='/google.svg' alt="Google Icon" />Continue with Google
-                </span>
-            </button>
-
+  return (
+    <div className="rounded-lg border-white border-opacity-10 border-2 p-6 max-w-lg w-full backdrop-blur-lg shadow-[0_0_15px_rgba(255,255,255,0.2)]">
+      <h2 className="text-2xl text-center mb-10 text-white font-bold">
+        Login to your account
+      </h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="relative mb-4">
+          <input
+            type="email"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              },
+            })}
+            className={`peer w-full p-3 border ${
+              errors.email ? "border-red-500" : "border-gray-400"
+            } rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
+            placeholder="Enter your email"
+          />
+          <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
+            Enter your Email{" "}
+            {errors.email && <span className="text-red-500">*</span>}
+          </label>
         </div>
-    )
-}
+
+        <div className="relative mb-4">
+          <input
+            type="password"
+            {...register("password", { required: "Password is required" })}
+            className={`peer w-full p-3 border ${
+              errors.password ? "border-red-500" : "border-gray-400"
+            } rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
+            placeholder="Enter your password"
+          />
+          <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
+            Enter your Password{" "}
+            {errors.password && <span className="text-red-500">*</span>}
+          </label>
+        </div>
+
+        {Object.keys(errors).length > 0 && (
+          <div className="mb-4 text-red-500 text-sm">
+            <p>
+              <span className="text-red-500">*</span> This is a required field
+            </p>
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="w-full bg-blue-800 text-white p-3 rounded-xl hover:bg-blue-950 transform duration-300 shadow-xl font-semibold"
+        >
+          Login
+        </button>
+      </form>
+
+      <div className="mt-4 flex justify-between text-sm mx-2">
+        <p className="text-zinc-300 underline hover:text-blue-500">
+          {" "}
+          Forgot Password
+        </p>
+        <p className="text-zinc-300">
+          Don't have an account?
+          <span
+            onClick={toggleForm}
+            className="text-blue-400 cursor-pointer hover:text-red-600 underline transition-colors duration-300 ml-1"
+          >
+            Sign Up
+          </span>
+        </p>
+      </div>
+
+      <div className="my-4 flex items-center">
+        <hr className="flex-grow border-t border-gray-300" />
+        <span className="px-4 text-gray-400">or</span>
+        <hr className="flex-grow border-t border-gray-300" />
+      </div>
+
+      <button className="w-full bg-sky-950 border border-gray-500 text-gray-100 p-2 rounded-lg hover:bg-blue-900 transform duration-300 shadow-2xl font-semibold">
+        <span className="flex justify-center items-center gap-2">
+          <img src="/google.svg" alt="Google Icon" />
+          Continue with Google
+        </span>
+      </button>
+      <Modal
+        title="Use Google authenticator"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+      >
+        <Space direction="horizontal" align="center">
+          <Title level={4}>Enter OTP</Title>
+          <Input.OTP value={OTP} onChange={() => setOtp(value)} />
+        </Space>
+      </Modal>
+    </div>
+  );
+};
 
 export default Login;
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,96 +1,262 @@
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  QRCode,
+  Modal,
+  Space,
+  Input,
+  Typography,
+  Flex,
+  notification,
+} from "antd";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+const { Title } = Typography;
 
 const Signup = ({ toggleForm }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [Code, setCode] = useState("");
+  const [OTP, setOtp] = useState(null);
+  const navigate = useNavigate();
+  const timeoutDuration = 60000; // 1 minute in milliseconds (60 seconds)
 
-    const onSubmit = (data) => {
-        console.log(data);
+  // Function that returns a Promise and waits for the OTP to be entered
+  const waitForOtp = () => {
+    return new Promise((resolve, reject) => {
+      // Start checking every 500ms if OTP is entered
+      const checkOtp = setInterval(() => {
+        // Convert OTP to a string and check if it has the required length
+        if (OTP.length === 6) {
+          clearInterval(checkOtp); // Stop checking once OTP is entered
+          clearTimeout(timeout); // Clear the timeout if OTP is entered in time
+          resolve(OTP); // Resolve the promise with the OTP value
+        }
+      }, 500);
+
+      // Set a timeout for 1 minute, after which the promise is rejected
+      const timeout = setTimeout(() => {
+        clearInterval(checkOtp); // Stop checking if the time runs out
+        reject(new Error("OTP input timed out after 1 minute")); // Reject with an error
+      }, timeoutDuration);
+    });
+  };
+
+  const GenerateQr = (code) => {
+    setCode(code);
+    setIsModalOpen(true);
+  };
+
+  const connectUrl = async (str, obj) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    let payload = {
+      email: obj.email,
+      password: obj.password,
+      name: obj.username,
+    };
+    const { data } = await axios.post(
+      `http://localhost:5000/api/auth/${str}`,
+      payload,
+      config
+    );
+    return data;
+  };
+
+  const QRcodeVerification = async (code) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    return (
-        <div className="rounded-lg border-white border-opacity-10 border-2 p-6 max-w-lg w-full backdrop-blur-lg shadow-[0_0_15px_rgba(255,255,255,0.2)]">"
-            <h2 className="text-2xl text-center mb-10 text-white font-bold">
-                Create a new account
-            </h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="relative mb-4">
-                    <input
-                        type="text"
-                        {...register('username', { required: 'Username is required' })}
-                        className={`peer w-full p-3 border ${errors.username ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
-                        placeholder="Enter your username"
-                    />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
-                        Enter your Username {errors.username && <span className="text-red-500">*</span>}
-                    </label>
-                </div>
+    // const { data } = await axios.post(
+    //   `http://localhost:5000/api/auth/QRverification`,
+    //   code,
+    //   config
+    // );
+    return false;
+  };
 
-                <div className="relative mb-4">
-                    <input
-                        type="email"
-                        {...register('email', {
-                            required: 'Email is required',
-                            pattern: {
-                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-                            }
-                        })}
-                        className={`peer w-full p-3 border ${errors.email ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
-                        placeholder="Enter your email"
-                    />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
-                        Enter your Email {errors.email && <span className="text-red-500">*</span>}
-                    </label>
-                </div>
+  const onSubmit = async (obj) => {
+    try {
+      // fetching the user in authentication db
+      const UserinAdb = await connectUrl("signup", obj);
+      console.log(UserinAdb);
 
-                <div className="relative mb-2">
-                    <input
-                        type="password"
-                        {...register('password', { required: 'Password is required' })}
-                        className={`peer w-full p-3 border ${errors.password ? 'border-red-500' : 'border-gray-400'} rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
-                        placeholder="Enter your password"
-                    />
-                    <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
-                        Enter your Password {errors.password && <span className="text-red-500">*</span>}
-                    </label>
-                </div>
+      // asking whether emial is verified
+      const userConfirmed = window.confirm(
+        "Have you verified though the link sended to your email?"
+      );
 
-                {Object.keys(errors).length > 0 && (
-                    <div className="mb-4 text-red-500 text-sm">
-                        <p><span className="text-red-500">*</span> This is a required field</p>
-                    </div>
-                )}
+      if (userConfirmed) {
+        // chking is email is verified
+        const isEmailVerified = await connectUrl("isEmailVerified", obj);
+        if (isEmailVerified) {
+          notification.success({
+            message: "1st Verification done!",
+            description: "Verified through email succesfuly!",
+          });
+          // to store user in db
+          //   const User = await connectUrl("storeUser", obj);
 
-                <button type='submit' className="w-full bg-blue-800 text-white p-3 rounded-xl hover:bg-blue-950 transform duration-300 shadow-xl font-semibold">
-                    Register
-                </button>
-            </form>
+          //2nd Verification
+          const FetchedCode = "";
+          GenerateQr(FetchedCode);
+          const enteredOtp = await waitForOtp();
+          const isQRverified = await QRcodeVerification(enteredOtp);
+          if (isQRverified) {
+            localStorage.setItem("userinfo", JSON.stringify(User));
+            notification.success({
+              message: "2nd Verification done!",
+              description: "Verified through QR succesfuly!",
+            });
+            navigate("/dashboard");
+          } else {
+            notification.error({
+              message: "QR Verification not done!",
+              description: "Please verify QR properly!",
+            });
+          }
+        } else {
+          notification.error({
+            message: "Verification not done!",
+            description: "Please verify email properly!",
+          });
+        }
+      } else {
+        // User clicked "Cancel"
+        notification.error({
+          message: "Verification not done!",
+          description: "Please verify link sended in your email",
+        });
+      }
+    } catch (error) {
+      notification.error({
+        message: "Error Occcured!",
+        description: error,
+      });
+    }
+  };
 
-            <div className="mt-4 text-center text-sm">
-                <p className="text-zinc-300">
-                    Already have an account?
-                    <span
-                        onClick={toggleForm}
-                        className="text-blue-400 cursor-pointer hover:text-red-600 underline transition-colors duration-300 ml-2">
-                        Login
-                    </span>
-                </p>
+  return (
+    <>
+      <div className="rounded-lg border-white border-opacity-10 border-2 p-6 max-w-lg w-full backdrop-blur-lg shadow-[0_0_15px_rgba(255,255,255,0.2)]">
+        "
+        <h2 className="text-2xl text-center mb-10 text-white font-bold">
+          Create a new account
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="relative mb-4">
+            <input
+              type="text"
+              {...register("username", { required: "Username is required" })}
+              className={`peer w-full p-3 border ${
+                errors.username ? "border-red-500" : "border-gray-400"
+              } rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
+              placeholder="Enter your username"
+            />
+            <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
+              Enter your Username{" "}
+              {errors.username && <span className="text-red-500">*</span>}
+            </label>
+          </div>
+
+          <div className="relative mb-4">
+            <input
+              type="email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                },
+              })}
+              className={`peer w-full p-3 border ${
+                errors.email ? "border-red-500" : "border-gray-400"
+              } rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
+              placeholder="Enter your email"
+            />
+            <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
+              Enter your Email{" "}
+              {errors.email && <span className="text-red-500">*</span>}
+            </label>
+          </div>
+
+          <div className="relative mb-2">
+            <input
+              type="password"
+              {...register("password", { required: "Password is required" })}
+              className={`peer w-full p-3 border ${
+                errors.password ? "border-red-500" : "border-gray-400"
+              } rounded-lg shadow-sm focus:border-blue-800 placeholder-transparent bg-transparent text-white mb-4 font-bold`}
+              placeholder="Enter your password"
+            />
+            <label className="absolute left-3 -top-5 text-sm font-medium text-gray-300 transition-all peer-placeholder-shown:top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-300 peer-focus:-top-5 peer-focus:left-2 peer-focus:text-sm peer-focus:text-white -z-10">
+              Enter your Password{" "}
+              {errors.password && <span className="text-red-500">*</span>}
+            </label>
+          </div>
+
+          {Object.keys(errors).length > 0 && (
+            <div className="mb-4 text-red-500 text-sm">
+              <p>
+                <span className="text-red-500">*</span> This is a required field
+              </p>
             </div>
+          )}
 
-            <div className="my-4 flex items-center">
-                <hr className="flex-grow border-t border-gray-300" />
-                <span className="px-4 text-gray-400">or</span>
-                <hr className="flex-grow border-t border-gray-300" />
-            </div>
-
-            <button className="w-full bg-sky-950 border border-gray-500 text-gray-100 p-2 rounded-lg hover:bg-blue-900 transform duration-300 shadow-2xl font-semibold">
-                <span className='flex justify-center items-center gap-2'>
-                    <img src='/google.svg' alt="Google Icon" />Continue with Google
-                </span>
-            </button>
-
+          <button
+            type="submit"
+            className="w-full bg-blue-800 text-white p-3 rounded-xl hover:bg-blue-950 transform duration-300 shadow-xl font-semibold"
+          >
+            Register
+          </button>
+        </form>
+        <div className="mt-4 text-center text-sm">
+          <p className="text-zinc-300">
+            Already have an account?
+            <span
+              onClick={toggleForm}
+              className="text-blue-400 cursor-pointer hover:text-red-600 underline transition-colors duration-300 ml-2"
+            >
+              Login
+            </span>
+          </p>
         </div>
-    )
-}
+        <div className="my-4 flex items-center">
+          <hr className="flex-grow border-t border-gray-300" />
+          <span className="px-4 text-gray-400">or</span>
+          <hr className="flex-grow border-t border-gray-300" />
+        </div>
+        <button className="w-full bg-sky-950 border border-gray-500 text-gray-100 p-2 rounded-lg hover:bg-blue-900 transform duration-300 shadow-2xl font-semibold">
+          <span className="flex justify-center items-center gap-2">
+            <img src="/google.svg" alt="Google Icon" />
+            Continue with Google
+          </span>
+        </button>
+      </div>
+      <Modal
+        title="Scan With Google authenticator"
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+      >
+        <Space direction="horizontal" align="center">
+          <QRCode value={Code || "-"} />
+          <Flex align="flex-end" vertical>
+            <Title level={4}>Enter OTP</Title>
+            <Input.OTP value={OTP} onChange={() => setOtp(value)} />
+          </Flex>
+        </Space>
+      </Modal>
+    </>
+  );
+};
 
 export default Signup;
